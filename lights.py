ce9334006de9089554ea61ff9394e27e10a39475
AlexLamson <alex.lamson@gmail.com>
2019-04-21 15:41:03 -0400

    re-enabled arguments

--- lights.py
+++ lights.py
@@ -1,7 +1,7 @@
 import time
 import sys
 import numpy as np
-# from serial_library import write
+from serial_library import write
 from color_library import color_names, hsv2rgb
 
 
@@ -15,34 +15,33 @@ if len(args) == 1:
     color_name = args[0]
     if color_name in color_names:
         rgb[:] = color_names[color_name]
-    pass
+    write(rgb)
+
 elif len(args) > 0 and len(args) % 3 == 0:
     a = np.array(list(map(int, args))).reshape(-1,3)
     indexes = np.arange(NUM_LEDS)*a.shape[0]//NUM_LEDS
     indexes = indexes.astype(int)
     rgb = a[indexes,:]
+    write(rgb)
 
+else:
+    def millis():
+        return int(round(time.time() * 1000))
 
-# write(rgb)
+    last_tick = 0
+    frame = 0
+    fps = 2
+    ms_per_frame = 1000/fps
+    while True:
+        current_tick = millis()
+        if current_tick - last_tick >= ms_per_frame:
+            last_tick = current_tick
+            frame += 1
 
-
-def millis():
-    return int(round(time.time() * 1000))
-
-last_tick = 0
-frame = 0
-fps = 2
-ms_per_frame = 1000/fps
-while True:
-    current_tick = millis()
-    if current_tick - last_tick >= ms_per_frame:
-        last_tick = current_tick
-        frame += 1
-
-        # rainbow
-        sec_per_cycle = fps*10  # number of seconds for each rainbow cycle
-        rgb = np.array( [hsv2rgb((frame % sec_per_cycle)/(sec_per_cycle-1), 1, 1)]*NUM_LEDS )
-        print(rgb[:2,:])
-        print(rgb.shape)
-#        write(rgb.astype(int))
-#        exit()
+            # rainbow
+            sec_per_cycle = fps*10  # number of seconds for each rainbow cycle
+            rgb = np.array( [hsv2rgb((frame % sec_per_cycle)/(sec_per_cycle-1), 1, 1)]*NUM_LEDS )
+            print(rgb[:2,:])
+            print(rgb.shape)
+            write(rgb.astype(int))
+            # exit()
